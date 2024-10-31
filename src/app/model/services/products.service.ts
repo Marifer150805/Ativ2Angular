00968c213fc34694dae65aff4146c46ae8fecdd3
Maria Fernanda <132359722+Marifer150805@users.noreply.github.com>
2024-10-31 16:26:48 -0300

    ProjAngular

--- src/app/model/services/products.service.ts
+++ src/app/model/services/products.service.ts
@@ -7,18 +7,18 @@ import { Iproducts } from './iproducts';
 export class ProductsService {
 
   private products: Iproducts[] = [
-    { id: 1, name: 'O medico e o monstro', price: 45.0, description: 'Robert Stevenson', image:'../image.png'},
-    { id: 2, name: 'O vilarejo', price: 20.5, description: 'Rafael Montes', image:'../image copy.png'},
-    { id: 3, name: 'Suicidas', price: 90.0, description: 'Rafael Montes', image:'../image copy 2.png'},
-    { id: 1, name: 'O medico e o monstro', price: 45.0, description: 'Robert Stevenson', image:'../image.png'},
-    { id: 2, name: 'O vilarejo', price: 20.5, description: 'Rafael Montes', image:'../image copy.png'},
-    { id: 3, name: 'Suicidas', price: 90.0, description: 'Rafael Montes', image:'../image copy 2.png'},
-    { id: 1, name: 'O medico e o monstro', price: 45.0, description: 'Robert Stevenson', image:'../image.png'},
-    { id: 2, name: 'O vilarejo', price: 20.5, description: 'Rafael Montes', image:'../image copy.png'},
-    { id: 3, name: 'Suicidas', price: 90.0, description: 'Rafael Montes', image:'../image copy 2.png'},
-    { id: 1, name: 'O medico e o monstro', price: 45.0, description: 'Robert Stevenson', image:'../image.png'},
-    { id: 2, name: 'O vilarejo', price: 20.5, description: 'Rafael Montes', image:'../image copy.png'},
-    { id: 3, name: 'Suicidas', price: 90.0, description: 'Rafael Montes', image:'../image copy 2.png'},
+    { id: 1, name: 'E assim que acaba', price: 45.0, description: 'Robert Stevenson', image:'../image.png'},
+    { id: 2, name: 'Não confie em ninguem', price: 20.5, description: 'Rafael Montes', image:'../imag1.jpg'},
+    { id: 3, name: 'Um de nos esta mentindo', price: 90.0, description: 'Rafael Montes', image:'../imag2.jpg'},
+    { id: 1, name: 'E assim que acaba', price: 45.0, description: 'Robert Stevenson', image:'../image.png'},
+    { id: 2, name: 'Não confie em ninguem', price: 20.5, description: 'Rafael Montes', image:'../imag1.jpg'},
+    { id: 3, name: 'Um de nos esta mentindo', price: 90.0, description: 'Rafael Montes', image:'../imag2.jpg'},
+    { id: 1, name: 'E assim que acaba', price: 45.0, description: 'Robert Stevenson', image:'../image.png'},
+    { id: 2, name: 'Não confie em ninguem', price: 20.5, description: 'Rafael Montes', image:'../imag1.jpg'},
+    { id: 3, name: 'Um de nos esta mentindo', price: 90.0, description: 'Rafael Montes', image:'../imag2.jpg'},
+    { id: 1, name: 'E assim que acaba', price: 45.0, description: 'Robert Stevenson', image:'../image.png'},
+    { id: 2, name: 'Não confie em ninguem', price: 20.5, description: 'Rafael Montes', image:'../imag1.jpg'},
+    { id: 3, name: 'Um de nos esta mentindo', price: 90.0, description: 'Rafael Montes', image:'../imag2.jpg'},
   ];
 
   getProducts(): Iproducts[]{
